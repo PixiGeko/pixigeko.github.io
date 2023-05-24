dd7e8936f1cc7f8951901c891c3e128660726a13
PixiGeko <pixigeko@gmail.com>
2023-05-24 13:06:03 +0200

    Add version query params

--- src/app/components/minecraft/datassets-download/datassets-download.component.ts
+++ src/app/components/minecraft/datassets-download/datassets-download.component.ts
@@ -3,6 +3,8 @@ import {MGDService} from "../../../services/mgd.service";
 import {MGDIndexVersion} from "../../../models/minecraft/mdg";
 import {KeyValue} from "@angular/common";
 import {MCMeta} from "../../../models/minecraft/mcmeta";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ArrayUtils} from "../../../utils/ArrayUtils";
 
 @Component({
   selector: 'app-datassets-download',
@@ -14,47 +16,69 @@ export class DatassetsDownloadComponent implements OnInit {
     assets: {},
     data: {}
   }
-  
+
   versions?: Record<string, MGDIndexVersion[]>;
 
-  constructor(private mgdService: MGDService) {
+  constructor(private mgdService: MGDService, private router: Router, private activatedRoute: ActivatedRoute) {
 
   }
 
   ngOnInit() {
     this.mgdService.index().subscribe({
       next: (index) => {
-        this.versions = this.groupBy(Object.values(index.versions).reverse(), v => v.version);
+        const indexVersion = Object.values(index.versions).reverse();
+        this.versions = ArrayUtils.groupBy(Object.values(indexVersion), v => v.version);
+
+        this.activatedRoute.queryParamMap.subscribe(params => {
+          if (params.has('version') && params.get('version') !== 'latest') {
+            const versionFound = indexVersion.find(v => v.id === params.get('version'));
+
+            if (versionFound) {
+              this.status.selectedVersion = versionFound;
+            } else {
+              this.status.selectedVersion = indexVersion[0];
+            }
+          } else {
+            this.status.selectedVersion = indexVersion[0];
+          }
+
+          this.selectedVersionChanged();
+        })
       }
     })
   }
-  
+
   selectedVersionChanged() {
-    if(!this.status.selectedVersion) return;
+    if (!this.status.selectedVersion) return;
+
     this.status.data = {};
     this.status.assets = {};
-    
+
     this.mgdService.loadAssetsMCMeta(this.status.selectedVersion).subscribe(mcmeta => this.status.assets.mcmeta = mcmeta);
     this.mgdService.loadDataMCMeta(this.status.selectedVersion).subscribe(mcmeta => this.status.data.mcmeta = mcmeta);
+
+    this.router.navigate(
+      [],
+      {
+        relativeTo: this.activatedRoute,
+        queryParams: {
+          version: this.status.selectedVersion.id
+        },
+        queryParamsHandling: 'merge'
+      }
+    );
   }
-  
+
   download(type: string) {
-    if(!this.status.selectedVersion) return;
+    if (!this.status.selectedVersion) return;
     window.open(`https://github.com/PixiGeko/Minecraft-default-${type}/archive/refs/heads/${this.mgdService.toVersionBranch(this.status.selectedVersion)}.zip`, '_blank')
   }
 
-  groupBy<T, K extends keyof any>(arr: T[], key: (i: T) => K) {
-    return arr.reduce((groups, item) => {
-      (groups[key(item)] ||= []).push(item);
-      return groups;
-    }, {} as Record<K, T[]>)
-  }
-
   reverse(a: KeyValue<string, MGDIndexVersion[]>, b: KeyValue<string, MGDIndexVersion[]>): number {
-    if(a.key === 'special') return 1;
+    if (a.key === 'special') return 1;
     return a.key > b.key ? -1 : (b.key > a.key ? 1 : 0);
   }
-  
+
   get canDownloadAssets() {
     return this.status.selectedVersion && this.status.assets.mcmeta && !this.status.assets.isLoading;
   }
